bccac0f1ebee31b3684905338829c7f0696f86cb
Krystian Carter <krystian.carter18@gmail.com>
2026-04-28 08:36:51 -0400

    Patched glyph ids

--- src/main/java/com/mystichorizons/mysticnametags/nameplate/packet/PacketGlyphIdFactory.java
+++ src/main/java/com/mystichorizons/mysticnametags/nameplate/packet/PacketGlyphIdFactory.java
@@ -5,6 +5,9 @@ import java.util.UUID;
 
 public final class PacketGlyphIdFactory {
 
+    private static final int FAKE_ID_BASE = 1_000_000_000;
+    private static final int FAKE_ID_RANGE = 1_000_000_000;
+
     private PacketGlyphIdFactory() {
     }
 
@@ -12,17 +15,27 @@ public final class PacketGlyphIdFactory {
                               int viewerNetworkId,
                               int lineIndex,
                               int glyphIndex) {
+        return fakeId(subjectUuid, viewerNetworkId, lineIndex, glyphIndex, 1);
+    }
+
+    public static int lineAnchorId(@Nonnull UUID subjectUuid,
+                                   int viewerNetworkId,
+                                   int lineIndex) {
+        return fakeId(subjectUuid, viewerNetworkId, lineIndex, -1, 2);
+    }
+
+    private static int fakeId(@Nonnull UUID subjectUuid,
+                              int viewerNetworkId,
+                              int lineIndex,
+                              int glyphIndex,
+                              int kind) {
         int hash = 17;
+        hash = 31 * hash + kind;
         hash = 31 * hash + subjectUuid.hashCode();
         hash = 31 * hash + viewerNetworkId;
         hash = 31 * hash + lineIndex;
         hash = 31 * hash + glyphIndex;
-        hash = Math.abs(hash);
 
-        if (hash == 0 || hash == Integer.MIN_VALUE) {
-            hash = 1;
-        }
-
-        return hash;
+        return FAKE_ID_BASE + Math.floorMod(hash, FAKE_ID_RANGE);
     }
 }
